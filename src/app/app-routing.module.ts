87788f75cc8def892f826b6e6e1de8f606a124a0
vitalii-martyniak <vitalii.martyniak@sombrainc.com>
2022-01-17 22:24:56 +0200

    Added views and layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LayoutComponent} from "./components/layout/layout.component";
+import {CatalogComponent} from "./views/catalog/catalog.component";
+import {CartComponent} from "./views/cart/cart.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LayoutComponent, children: [
+      {path: '', redirectTo: 'catalog', pathMatch: 'full'},
+      {path: 'catalog', component: CatalogComponent},
+      {path: 'cart', component: CartComponent},
+      {path: '**', redirectTo: '/'}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { CatalogComponent } from './views/catalog/catalog.component';
+import { CartComponent } from './views/cart/cart.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    CatalogComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
